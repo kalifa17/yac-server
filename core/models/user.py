d28852c405614840efbef1a35c49d7c33c5b4d4b
Mauricio Nomesque <kalifa17@gmail.com>
2018-07-16 08:37:39 -0500

    yac server

---
+++ core/models/user.py
@@ -0,0 +1,29 @@
+
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+from django.utils import timezone
+from .message import Message
+
+# from core.models.conversation import Conversation
+
+class User(AbstractUser):
+
+    last_read_date = models.DateTimeField(
+        auto_now_add=True,
+        blank=False,
+        null=False
+    )
+    online = models.BooleanField(null=False, blank=False, default=False)
+
+    REQUIRED_FIELDS = []
+
+    def __str__(self):
+        return self.username
+
+    def read(self):
+        self.last_read_date = timezone.now()
+        self.save()
+
+    def unread_messages(self):
+        return Message.objects.filter(created_at__gt=self.last_read_date) \
+                              .count()
